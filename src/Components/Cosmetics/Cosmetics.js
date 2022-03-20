ef1a9e7c2ab9ece0e6bf5b4da250fcd111061cfc
sumon092 <m.sumon@outlook.com>
2022-03-21 00:22:55 +0600

    initialize cosmetics store

---
+++ src/Components/Cosmetics/Cosmetics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cosmetics = () => {
+    return (
+        <div>
+            <h2>Welcome to cosmetics store</h2>
+        </div>
+    );
+};
+
+export default Cosmetics;
